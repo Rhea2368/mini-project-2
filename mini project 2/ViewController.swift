d986dd350c5166282b21d37564dd1bea762996f4
Rhea <rheadk8@icloud.com>
2020-07-24 22:07:16 +0800

    Initial Commit

---
+++ mini project 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mini project 2
+//
+//  Created by Rhea Kundamal on 24/7/2020.
+//  Copyright © 2020 Rhea Kundamal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
