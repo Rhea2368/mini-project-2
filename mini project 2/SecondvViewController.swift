ca61161c48bd1b15202d873125441238bf584884
Rhea <rheadk8@icloud.com>
2020-07-28 23:27:03 +0800

    commit

---
+++ mini project 2/SecondvViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondvViewController.swift
+//  mini project 2
+//
+//  Created by Rhea Kundamal on 24/7/2020.
+//  Copyright © 2020 Rhea Kundamal. All rights reserved.
+//
+
+import UIKit
+
+class SecondvViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
